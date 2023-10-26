bf3ac0d15e5cea62ca9b91ba67ee09e1c16a8387
Jerrit Gläsker <jrgls.no-reply@gmx.de>
2023-10-26 15:07:45 -0700

    Added icon conversion

--- process-viewer/View/Bluetooth/BTDeviceRow.swift
+++ process-viewer/View/Bluetooth/BTDeviceRow.swift
@@ -7,12 +7,33 @@
 
 import SwiftUI
 
+let ICON_MAP: Dictionary<String, String> = [
+    "audio-card": "waveform",
+    "audio-headphones": "headphones",
+    "audio-headset": "headphones",
+    "camera-photo": "camera",
+    "camera-video": "video",
+    "computer": "desktopcomputer",
+    "input-gaming": "gamecontroller",
+    "input-keyboard": "keyboard",
+    "input-mouse": "computermouse",
+    "input-tabled": "ipad.landscape",
+    "modem": "wifi.router",
+    "multimedia-player": "play.square.stack.fill",
+    "network-wireless": "antenna.radiowaves.left.and.right",
+    "phone": "iphone",
+    "printer": "printer",
+    "scanner": "scanner",
+    "video-display": "play.display",
+    "unknown": "camera.metering.unknown"
+]
+
 struct BTDeviceRow: View {
     var device: BTDevice
     
     var body: some View {
         HStack {
-            Image(systemName: "antenna.radiowaves.left.and.right")
+            Image(systemName: ICON_MAP[device.icon] ?? "questionmark")
             Text("\(device.name)")
         }
     }
